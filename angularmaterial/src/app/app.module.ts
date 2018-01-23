714362f178d3911c45f2e58810730b68466f633b
Sabadis <dan.sabadis@siemens.com>
2018-01-23 17:20:28 +0200

    arrived at avatar add

--- angularmaterial/src/app/app.module.ts
+++ angularmaterial/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MaterialModule } from './shared/material.module';
 
--- angularmaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ angularmaterial/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, NgZone } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../../models/user';
 import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
@@ -11,16 +15,23 @@ const SMALL_WIDTH_BREAKPOINT = 720;
 export class SidenavComponent implements OnInit {
 
   private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
+  users: Observable<User[]>;
 
   constructor(
     zone: NgZone,
-    //private userService: UserService,
+    private userService: UserService,
     private router: Router) {
     this.mediaMatcher.addListener(mql =>
       zone.run(() => this.mediaMatcher = mql));
   }
 
   ngOnInit() {
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.users.subscribe(data => {
+      console.log(data);
+    });
   }
 
   isScreenSmall(): boolean {
